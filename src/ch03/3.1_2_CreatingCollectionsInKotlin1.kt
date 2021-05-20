c7a8df095b1c95ccdcbff85b8ac3462106ec9063
Johannes Hoffmann <27806916+j-m-hoffmann@users.noreply.github.com>
2021-05-20 18:59:23 +0200

    fix: use OrNull functions

--- src/ch03/3.1_2_CreatingCollectionsInKotlin1.kt
+++ src/ch03/3.1_2_CreatingCollectionsInKotlin1.kt
@@ -1,8 +1,8 @@
 package ch03.ex1_2_CreatingCollectionsInKotlin1
 
-fun main() {
+fun main(args: Array<String>) {
     val strings = listOf("first", "second", "fourteenth")
     println(strings.last())
     val numbers = setOf(1, 14, 2)
-    println(numbers.max())
+    println(numbers.maxOrNull() ?: 0)
 }
